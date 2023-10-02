447855dad695eed827e8f5e7124815161ec60734
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-01 21:38:27 -0300

    primeras vistas

---
+++ app/src/main/java/com/example/androidmarkettest/ShoppingCartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidmarkettest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingCartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
